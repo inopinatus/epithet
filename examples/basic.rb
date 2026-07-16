5900c4b1729d06421dff530eccebcbb6067004e3
Josh Goodall <inopinatus@hey.com>
2026-07-16 22:12:36 +0200

    Use CSPRNG in example

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
+require 'securerandom'
 require 'epithet'
 
 # Using a random passphrase means that epithet identifiers are effectively
 # ephemeral, since decoding is limited to the lifetime of this process.
 def epithet_initialize
   Epithet.configure(
-    passphrase: ENV.fetch('EPITHET_PASSPHRASE') { Random.bytes(32) },
+    passphrase: ENV.fetch('EPITHET_PASSPHRASE') { SecureRandom.random_bytes(32) },
     scrypt: { salt: 'myapp/production' },
     context: 'v1'
   )
