ddb813d4a065fda3efce202276b9c2017a14d7bb
Josh Goodall <inopinatus@hey.com>
2026-07-22 19:10:14 +0200

    improve keygen

--- lib/epithet/keygen.rb
+++ lib/epithet/keygen.rb
@@ -25,17 +25,27 @@ class Epithet
       length: 32
     }.freeze
 
-    # Create a new key generator from either high-entropy key material, or a supplied passphrase.
-    # Supplied scrypt params, if any, are merged over DEFAULT_SCRYPT_PARAMS; `provider:` may name
-    # a Scrypt provider class, otherwise one is selected automatically.
+    # Create a new key generator from a supplied passphrase, or from high-entropy initial key
+    # material if already prepared.  The passphrase will be hashed with scrypt.  Supplied scrypt
+    # params, if any, are merged over DEFAULT_SCRYPT_PARAMS, so this works:
+    #
+    #     Epithet::Keygen.new(
+    #       passphrase: ENV.fetch('EPITHET_PASSPHRASE'),
+    #       scrypt: { salt: "#{MyApp.name}/#{MyApp.env}" }
+    #     )
+    #
+    # A scrypt provider will be chosen by `Epithet::Scrypt.auto`.  To override automatic selection
+    # and use a specific scrypt provider, pass it as `provider` in the scrypt parameters:
+    #
+    #     kg = Epithet::Keygen.new(passphrase: 'pw', scrypt: { provider: Epithet::Scrypt::OpenSSL })
+    #
+    # but this should be unnecessary in the common case.
     def initialize(ikm: nil, passphrase: nil, digest: 'sha256', scrypt: {})
-      if (passphrase.nil? && ikm.nil?) || (!passphrase.nil? && !ikm.nil?)
+      unless passphrase.nil? ^ ikm.nil?
         raise ArgumentError, 'keygen requires either ikm or passphrase'
       end
 
-      params = DEFAULT_SCRYPT_PARAMS.merge(Hash(scrypt))
-      scrypt = (params.delete(:provider) || Scrypt.auto).new(**params)
-      @ikm = (ikm&.b || scrypt.ikm(passphrase)).freeze
+      @ikm = (ikm&.b || build_scrypt(Hash(scrypt)).ikm(passphrase)).freeze
       @digest = -String(digest)
       freeze
     end
@@ -47,6 +57,13 @@ class Epithet
     # Derive a key via HKDF.
     def generate(info, salt, length)
       OpenSSL::KDF.hkdf(@ikm, hash: @digest, info:, salt:, length:)
+    end
+
+    private
+
+    def build_scrypt(opts)
+      params = DEFAULT_SCRYPT_PARAMS.merge(opts)
+      (params.delete(:provider) || Scrypt.auto).new(**params)
     end
   end
 end
